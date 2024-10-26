cfeae982221e28f54893f5eb9c6e2d30b7b403ec
Djon777 <sorokin.zhenekMLE@yandex.ru>
2024-10-26 21:56:48 +0300

    level 3-3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,7 +1,8 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest(a, b, i) {
-
-    return (a < b, b < i)
+function findLargest(a, b, c) {
+    const nan = (a === 1, b === 2, c === 3);
+    nan = c;
+    return nan
     
 }
