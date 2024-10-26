6d45006cb47439174dc387788c9c6666129b54d3
Djon777 <sorokin.zhenekMLE@yandex.ru>
2024-10-26 23:51:03 +0300

    level 3-2

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -2,7 +2,7 @@
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
 function isEven(a) {
-if (a / 2 === 0) {
+if (a % 2 === 0) {
     return true
 } else {
     return false
