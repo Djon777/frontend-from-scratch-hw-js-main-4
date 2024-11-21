f4222dc74edcd20a36a1a896e0b5f3aea9e83175
Djon777 <sorokin.zhenekMLE@yandex.ru>
2024-11-21 22:56:03 +0300

    level6-1

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -17,3 +17,25 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+let caunt = 0
+const batom1 = document.getElementById("#prev-button")
+const batom2 = document.getElementById("#next-button")
+batom1.addEventListener("click", function(){
+  caunt --
+  if (caunt < 0) {
+    caunt = 2;
+  }
+    const imdD = document.querySelector("#web-tech-image")
+  imdD.setAttribute("src", WEB_TECH_IMAGES[caunt])
+})
+    batom2.addEventListener("click", function(){
+      caunt ++
+  if (caunt > 2) {
+    caunt = 2;
+  }
+  const imdD = document.querySelector("#web-tech-image")
+  imdD.setAttribute("src", WEB_TECH_IMAGES[caunt])
+    })
+
+
+
